e04f6018e9c04f9c9a111f65d5e5d825cd2d8b1d
Nikhil Jose <75926880+nikhiljose-123@users.noreply.github.com>
2023-05-24 16:58:05 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./App.css"
+import UserHome from "./Pages/UserHome";
+
+function App() {
+  
+  
+  return (
+    <div className="app">
+    
+
+     <UserHome/>
+     
+     
+     
+     
+    
+    </div>
+    
+   
+  );
+
+  }
+export default App;
